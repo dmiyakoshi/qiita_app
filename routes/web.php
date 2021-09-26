5712e86a1713ec0f33c275d16fdc3047cf247244
dmiyakoshi <d1m354dai@gmail.com>
2021-09-26 09:03:31 +0900

    ログイン機能追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::resource('articles', App\Http\Controllers\ArticleController::class);
+
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
